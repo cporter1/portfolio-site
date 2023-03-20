1a9a49d43107cb3400ffdef0a8741e2d4cda052b
Colin Porter <coln.porter@gmail.com>
2023-03-20 15:28:29 -0700

    added resume download functionality

--- src/pages/sections/AboutMe.js
+++ src/pages/sections/AboutMe.js
@@ -1,4 +1,4 @@
-import resume from '@/images/resume.jpeg'
+const resume = require('@/images/resume.jpeg')
 
 import soccer from '@/images/soccer-ball.svg'
 import compass from '@/images/compass.svg'
@@ -7,20 +7,32 @@ import computer from '@/images/computer.svg'
 import Image from 'next/image'
 
 export default function AboutMe(props) {
+
+  function downloadResume() {
+
+    const element = document.createElement("a");
+    element.href = resume.default.src
+    element.download = 'cporterresume.jpeg'
+
+    document.body.appendChild(element);
+
+    element.click();
+  }
+
   return (
     <section id='about-section' className="about-container">
-      <h2>About Me</h2>
+      <h2>ABOUT ME</h2>
       <div className="about-grid-wrapper">
         <div className="about-column1-container">
           <div className='paragraph-wrapper'>
             <p id='top'>
-              Hello, my name is Colin Porter and I am a full stack software engineer with a focus in frontend.
+              Hello, my name is Colin Porter and I am a full stack software engineer with a specialty in frontend.
             </p>
             <p>I have 4+ years of experience building intuitive and feature rich production web applications.</p>
             <p>I have a passion for producing dynamic applications that put the user experience at the forefront of development.</p>
             <p>I am currently job hunting, send me a message if you think I&apos;d be a good fit on your company.</p>
             <h3><i>A little bit more...</i></h3>
-            <p>I love going backpacking, playing board games with friends, and everything about soccer!</p>
+            <p>I love playing board games with friends, going hiking & backpacking, and everything about soccer!</p>
             <div className='icon-wrapper'>
               <Image className='hobby-icon' src={computer} alt=''/>
               <Image className='hobby-icon' src={compass} alt=''/>
@@ -31,7 +43,10 @@ export default function AboutMe(props) {
         <div className="about-column2-container">
           <div className='resume-wrapper'>
             <Image className='resume' src={resume} alt=''/>
-            <div className='resume-download-button'/>
+
+            <div className='download-button-wrapper'>
+              <div onClick={downloadResume} className='resume-download-button'/>
+            </div>
           </div>
         </div>
       </div>
