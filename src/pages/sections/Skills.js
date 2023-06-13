2a529876b42dfe42e0e0ff8dae61ae8710b06cbe
Colin Porter <coln.porter@gmail.com>
2023-06-13 15:07:37 -0700

    skills sections updated

--- src/pages/sections/Skills.js
+++ src/pages/sections/Skills.js
@@ -14,8 +14,12 @@ export default function Skills() {
             <li>Nginx</li>
             <li>HTML5</li>
             <li>CSS3</li>
+            <li>SASS</li>
             <li>MySQL</li>
             <li>PostgreSQL</li>
+            <li>Webpack</li>
+            <li>tRPC</li>
+            <li>Typescript</li>
           </ul>
         </div>
         <div className="skills-column2">
@@ -28,14 +32,17 @@ export default function Skills() {
             <li>NPM</li>
             <li>Yarn</li>
             <li>Postman</li>
+            <li>AWS EC2</li>
+            <li>AWS S3</li>
+            <li>AWS VPC</li>
           </ul>
         </div>
         <div className="skills-column3">
           <h3>SOFT</h3>
           <ul className="soft-skills">
-            <li>Strong communicator and public speaker with 3 years of team building certifications</li>
-            <li>Adept learner who seeks out criticism and delivers results on time</li>
-            <li>Meticulous planner and documenter who considers all edge cases</li>
+            <li>Strong communicator and public speaker with 3 years of team building certifications.</li>
+            <li>Adept learner who seeks out criticism and delivers results on time.</li>
+            <li>Meticulous planner and documenter who plans for all edge cases.</li>
           </ul>
         </div>
       </div>
